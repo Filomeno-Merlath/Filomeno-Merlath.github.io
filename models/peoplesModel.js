ede15a1d1fc1e75a0633b30cfe62b0d2bda625e6
filomeno merlath <filo.meno.junior@hotmail.com>
2021-12-10 22:20:27 +0000

    last update

--- models/peoplesModel.js
+++ models/peoplesModel.js
@@ -1,13 +1,13 @@
-var pool =  require("./connection");
+var pool = require("./connection");
 
-module.exports.getAllPeoples = async  function() {
-    try {
-        let sql = "select * from people";
-        let result = await pool.query(sql);
-        let people = result.rows;
-        return {status:200, result: people};
-    } catch (error) {
-        console.log(error);
-        return{status:500, result:error};
-    }
-}
+module.exports.getAllPeoples = async function () {
+  try {
+    let sql = "select * from people ORDER BY people_name";
+    let result = await pool.query(sql);
+    let people = result.rows;
+    return { status: 200, result: people };
+  } catch (error) {
+    console.log(error);
+    return { status: 500, result: error };
+  }
+};
